313f629bbc0742ef0f1e0a9d6fe1b76060cd6250
Björn Jürgens <juergens@teco.edu>
2018-03-21 15:25:31 +0100

    fixes for at-examples

--- examples/at_notebook/run.sh
+++ examples/at_notebook/run.sh
@@ -8,3 +8,10 @@ GIT=$(which git)
 unset GIT_DIR
 ${GIT} add --all .
 ${GIT} commit -m "done!"
+
+# turn this branch into master for convinient pull later
+branch=$(${GIT} rev-parse --abbrev-ref HEAD)
+${GIT} checkout master
+${GIT} reset --hard $branch
+
+#alternative: git branch -M $branch master
